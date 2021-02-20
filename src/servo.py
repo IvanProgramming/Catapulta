0e1694285160e78097a050331f215d9bf45eeac1
Ivan <vlasov.ivan.2004@gmail.com>
2021-02-20 17:02:53 +0300

    Improvements made with real model

--- src/servo.py
+++ src/servo.py
@@ -15,7 +15,7 @@ class Servo:
             self.servo_pwm = GPIO.PWM(self.pin, 50)
 
         self.servo_pwm.start(0)
-        dutyCycle = angle / 18. + 3.
+        dutyCycle = angle / 20 + 3.
         self.servo_pwm.ChangeDutyCycle(dutyCycle)
         sleep(0.7)
         if not hold:
@@ -28,8 +28,22 @@ if __name__ == '__main__':
     servo = Servo(14)
     servo2 = Servo(15)
 
-
-    sleep(3)
+    servo.set_servo_angle(-15, hold=True)
+    for angle in range(-15, 130):
+        duty_cycle = angle / 18 + 3.
+        servo.servo_pwm.ChangeDutyCycle(duty_cycle)
+        sleep(0.007)
+    sleep(1.5)
+    servo.set_servo_angle(127, hold=True)
+    servo2.set_servo_angle(0)
+    servo2.set_servo_angle(-30)
+    for angle in range(127, -15, -1):
+        duty_cycle = angle / 18 + 3.
+        servo.servo_pwm.ChangeDutyCycle(duty_cycle)
+        sleep(0.007)
+    servo.set_servo_angle(-15)
+    servo2.set_servo_angle(0)
+    exit()
 
 
     while True:
